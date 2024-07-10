9293576790405ec2c1f0d719e465eaccfbd0f61e
NNTkmutnb <oleslg40102@gmail.com>
2024-07-10 15:17:41 +0700

    Ex-02, Finish!!

--- Lecture-02/Ex-02.js
+++ Lecture-02/Ex-02.js
@@ -1,3 +1,8 @@
-function equation(num) {
-    
-}
+function equation(str) {
+   const calculate = new Function('return ' + str);
+   return calculate();
+};
+
+console.log(equation("1+1")); // => 2
+console.log(equation("7*4-2")); // => 26
+console.log(equation("1+1+1+1+1")); // => 5
